2057f8c9e0c8b41a80005b209f707ddc9547773d
greasedgab12 <zerodeux@gmx.de>
2017-12-29 15:23:15 +0100

    Cleanup, commenting and added mapSprite Prototype.

--- master/sprite.h
+++ master/sprite.h
@@ -11,10 +11,6 @@
 typedef struct Sprite_Struct* Sprite;
 typedef struct Blank_Struct* Blank;
 
-
-
-
-
 /*The byte-patterns for the sprite struct have the following shape: {lx, lx*ly, data...} */
 //const uint8_t player[] PROGMEM = {4,8, 0xFF, 0xFF, 0x0C,0x30,0x30,0x0C,0xC0,0x03};
 
@@ -42,17 +38,46 @@ struct Blank_Struct
     uint8_t ly;
 };
 
-void spriteInit();
-
+/* newSprite: allocate memory for a sprite with initial data.
+ * uint8_t x: x-coordinate of sprite
+ * uint8_t y: y-coordinate of sprite
+ * uint8_t *data: pointer to data array in memory
+ * return Sprite: returns pointer to newly allocated sprite.
+ */
 Sprite newSprite(uint8_t x, uint8_t y, uint8_t *data);
 
+/* Setter methods for Sprite
+ * setSpriteXY: Change position values of Sprite. This is does not equal moving the sprite.
+ */
 void setSpriteXY(Sprite self, uint8_t x, uint8_t y);
 void setSpriteData(Sprite self, uint8_t *data);
 
+/* delSprite: Remove Sprite from spriteList and add deletion entry to delList
+ * Sprite instance: Sprite to be deleted
+ */
 void delSprite(Sprite instance);
+
+/* moveSprite: Move Sprite to new Position. Calls delSprite and moveSprite in succession.
+ * Sprite instance: Sprite to be moved
+ */
 void moveSprite(Sprite instance, uint8_t nx, uint8_t ny);
+
+/* drawSprite: Add Sprite to drawList.
+ * Sprite instance: Sprite to be drawn.
+ */
 void drawSprite(Sprite instance);
 
+/* mapSprite: Map the sprite data to the pages of the display.
+ * Sprite instance: Sprite to be mapped to pages.
+ * return Sprite: Resulting mapped Sprite
+ */
+Sprite mapSprite(Sprite instance);
+
+/* newBlank: create Blank from Sprite.
+ * Sprite instance: Sprite to be used as pattern.
+ * return Blank: Generated instance of Blank.
+ */
 Blank newBlank(Sprite instance);
+
 
 #endif // SPRITE_H_INCLUDED
