47165fc76acf28924a5b7a63da55543a2cfba31a
greasedgab12 <zerodeux@gmx.de>
2018-01-03 14:27:26 +0100

    Defined new Block and Object structures.
    Object unifies the functionality of the Sprite structure while adding
    capabilities of an entity which i.e. reacts to input.
    Block effectivly a List of adjacent drawable pages.

---
+++ master/block.h
@@ -0,0 +1,22 @@
+/*
+ * block.h
+ *
+ *  Created on: Jan 3, 2018
+ *      Author: root
+ */
+
+#ifndef MASTER_BLOCK_H_
+#define MASTER_BLOCK_H_
+
+typedef struct Block_struct* Block;
+struct Block_struct{
+	uint8_t x,y,lx,ly;
+	uint8_t *data;
+};
+//Create new Block from Object instance
+Block newBlock(Object instance);
+//Create new Block from existing Data ( in case of overlap).
+Block newBlock(uint8_t x, uint8_t y, uint8_t *data);
+
+
+#endif /* MASTER_BLOCK_H_ */
---
+++ master/object.h
@@ -0,0 +1,46 @@
+/*
+ * object.h
+ *
+ *  Created on: Jan 3, 2018
+ *      Author: root
+ */
+#include "sprite.h"
+#ifndef MASTER_OBJECT_H_
+#define MASTER_OBJECT_H_
+
+typedef struct Object_Struct* Object;
+
+struct Object_Struct{
+	//attributes
+	uint8_t x,y,lx,ly;
+	const uint8_t *data;
+	Block representation;
+	//Pointer to Object data aside from Position and Graphics i.e. HP of player.
+	void* objectEnv;
+	//Type of the Object. See Objectlist for types
+	uint8_t type;
+
+	//methods
+    void (*setXY)(Object self, uint8_t x, uint8_t y);
+    void (*setData)(Object self, const uint8_t *data);
+    void (*think)(Object self, struct Environment *mainEnv);
+    void (*move)(Object self, uint8_t x, uint8_t y);
+    void (*collide)(Object self, Object other);
+};
+/* newObject: allocate memory for a sprite with initial data.
+ * uint8_t x: x-coordinate of sprite
+ * uint8_t y: y-coordinate of sprite
+ * uint8_t *data: pointer to data array in memory
+ * return Object: returns pointer to newly allocated Object.
+ */
+Object newObject(uint8_t x, uint8_t y, uint8_t type);
+
+/* Setter methods for Sprite
+ * setSpriteXY: Change position values of Sprite. This is does not equal moving the sprite.
+ */
+void setSpriteXY(Object self, uint8_t x, uint8_t y);
+void setSpriteData(Object self, const uint8_t *data);
+
+
+
+#endif /* MASTER_OBJECT_H_ */
