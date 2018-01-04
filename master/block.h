7c5d723c3ed8faa66b4766efbf771869645e22c3
greasedgab12 <zerodeux@gmx.de>
2018-01-04 10:54:20 +0100

    Added more Documentation

--- master/block.h
+++ master/block.h
@@ -11,8 +11,19 @@ struct Block_struct{
 	uint8_t *data;
 };
 
-//Create new Block from existing Data ( in case of overlap).
+/* newBlock: Create new Block from existing Data ( in case of overlap).
+ * uint8_t x: x coordinate of new Block.
+ * uint8_t y: y coordinate of new Block.
+ * uint8_t *data: bitmap of new Block.
+ * return Block: newly allocated Block from parameters.
+*/
 Block newBlock(uint8_t x, uint8_t y, uint8_t *data);
 
+/* checkBlockCollision: Check list of Blocks for collisions.
+ * Block* blockList: List of Blocks for collision check.
+ * uint8_t length: Length of that list.
+ * return Block*: List of fused overlapping Blocks.
+*/
+Block* checkBlockCollision(Block* blockList, uint8_t length);
 
 #endif /* MASTER_BLOCK_H_ */
