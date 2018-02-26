0ddb0672f9f0e2d9dab0167fbcd14072a114421b
sieglein <sieglein@sieglein-laptop>
2018-02-26 12:37:06 +0100

    added the ability to output funny messages and wierd numbers on the display

---
+++ master/char.h
@@ -0,0 +1,14 @@
+#ifndef CHARS_H
+#define CHARS_H
+#include <avr/pgmspace.h>
+#include <inttypes.h>
+
+//contains all chars
+extern const uint8_t alphabet[42][14] PROGMEM;
+
+void print(const char *text,uint8_t x,uint8_t y);
+void printN(const uint16_t *number,uint8_t x,uint8_t y);
+void printB(const uint8_t *number,uint8_t x,uint8_t y);
+
+
+#endif
